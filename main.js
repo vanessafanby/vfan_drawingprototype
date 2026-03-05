cfcd94215e4766874869c264ffa6ae337de4627f
Patrick McMahon <patrick.mcmahon@rmit.edu.au>
2026-03-05 16:20:05 +1100

    added stage and circle

---
+++ main.js
@@ -0,0 +1,18 @@
+// create the konva stage
+const stage = new Konva.Stage({
+    container: "konva-stage",
+    width: 500,
+    height: 500
+});
+
+const firstLayer = new Konva.Layer();
+
+const circle = new Konva.Circle({
+    x: 250,
+    y: 250,
+    radius: 50,
+    fill: "red"
+});
+
+firstLayer.add(circle);
+stage.add(firstLayer);
